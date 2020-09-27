067f0d84d352702eae81257659004ad230856078
Joshua Lowe <joshualowe1002@gmail.com>
2020-09-27 16:13:54 +0100

    Modal dark theme

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -2,12 +2,12 @@
 <div class="fixed z-10 inset-0 overflow-y-auto" v-if="open">
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
     <div class="fixed inset-0 transition-opacity">
-      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+      <div class="absolute inset-0 bg-gray-500 opacity-75 dark:bg-gray-600"></div>
     </div>
 
     <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
-    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full dark:bg-gray-900" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 dark:bg-gray-900">
         <div class="sm:flex sm:items-start">
           <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
             <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -15,11 +15,11 @@
             </svg>
           </div>
           <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+            <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-white" id="modal-headline">
               Delete Article
             </h3>
             <div class="mt-2">
-              <p class="text-sm leading-5 text-gray-500">
+              <p class="text-sm leading-5 text-gray-500 dark:text-gray-300">
                 Are you sure you want to delete this article? The article will be permanently removed. This action cannot be undone.
               </p>
             </div>
@@ -36,7 +36,7 @@
           </button>
         </span>
         <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-          <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5" @click="closeDelete">
+          <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white dark:bg-gray-800 dark:text-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5" @click="closeDelete">
             Cancel
           </button>
         </span>
