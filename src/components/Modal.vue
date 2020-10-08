cb799936db42f1ca54019b5f98e862a21d3a4b13
Joshua Lowe <joshualowe1002@gmail.com>
2020-10-08 20:59:39 +0100

    Fix CSS bugs

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -10,10 +10,10 @@
         <div class="bg-white px-3 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
             <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 class="text-xl   leading-6 font-medium text-gray-900" id="modal-headline">
-                {{title}}
+                <h3 class="text-xl leading-6 font-medium text-gray-900" id="modal-headline">
+                    {{title}}
                 </h3>
-                <div class="mt-6">
+                <div class="mt-6 text-gray-900">
                     <slot name="main" />
                 </div>
             </div>
